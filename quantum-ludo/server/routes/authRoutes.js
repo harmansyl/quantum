136deef73d8ba97fa882bbf9907922dce0a1d4f9
Quantum Ludo Developer <quantum-ludo@gmail.com>
2026-04-20 01:57:37 +0530

    Fix named import in authRoutes

--- quantum-ludo/server/routes/authRoutes.js
+++ quantum-ludo/server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import supabase from "../supabaseClient.js";
+import { supabase } from "../supabaseClient.js";
 
 const router = express.Router();
 
